5d773746d1278a03e02c338ac6152f14ae925fae
Andrew Joy Hembrom <135138539+AndrewHembrom@users.noreply.github.com>
2025-01-03 22:17:24 +0530

    supabase

--- server/__tests__/index.test.ts
+++ server/__tests__/index.test.ts
@@ -3,6 +3,12 @@ import app from "../src/index";
 import request from "supertest";
 
 describe("Test index.ts", () => {
+
+  test("Home Page route", async () => {
+    const res = await request(app).post("/po");
+    expect(res.body).toEqual({ message: "Hello World!" });
+  });
+
   test("Home Page route", async () => {
     const res = await request(app).get("/");
     expect(res.body).toEqual({ message: "Hello World!" });
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,41 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import { createClient } from '@supabase/supabase-js'
+import { Database } from './database.types'
+
+const supabase = createClient<Database>(
+  "https://mgdkoltzkakqquyohwkx.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1nZGtvbHR6a2FrcXF1eW9od2t4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzU4MDA2MjksImV4cCI6MjA1MTM3NjYyOX0.fmTXPQYB32FtAeX14J5APMaaFv8k82h3CMv1w--bKsw"
+)
 
 dotenv.config();
 const app = express();
 
 const PORT = process.env.PORT;
 
-app.get('/', (req, res) => {
-    res.json({message: "Hello World!"});
+app.post('/po', async (req, res) => {
+    const { error } = await supabase
+        .from('movies')
+        .insert({ id: 1, name: 'Denmark' })
+    
+    res.json({
+        message: "Hello World!",
+        error: error
+     });
+
+})
+
+app.get('/', async (req, res) => {
+
+    const { data, error } = await supabase
+        .from('movies')
+        .select()
+
+    res.json({
+        message: "Hello World!",
+        data: data
+     });
+    
 });
 
 app.listen(PORT, () => {
